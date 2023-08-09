dc3d590a01ce1bcc632be214d9feeca4cba94399
jmrojo98 <77588880+jmrojo98@users.noreply.github.com>
2023-08-09 23:49:29 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,42 @@
+function skillmember()
+    var skill = document.getElementById("skill").value;
+    var member = document.getElementById("member").value;
+    var result = document.getElementById("result");
+    if (skill == "Java" && member == "1") {
+        result.value = "2000";
+    } else if (skill == "Java" && member == "2") {
+        result.value = "4000";
+    } else if (skill == "Java" && member == "3") {
+        result.value = "6000";
+    } else if (skill == "Java" && member == "4") {
+        result.value = "8000";
+    } else if (skill == "Java" && member == "5") {
+        result.value = "10000";
+    } else if (skill == "Java" && member == "6") {
+        result.value = "12000";
+    } else if (skill == "Java" && member == "7") {
+        result.value = "14000";
+    } else if (skill == "Java" && member == "8") {
+        result.value = "16000";
+    } else if (skill == "Java" && member == "9") {
+        result.value = "18000";
+    } else if (skill == "Java" && member == "10") {
+        result.value = "20000";
+    } else if (skill == "Java" && member == "11") {
+        result.value = "22000";
+    } else if (skill == "Java" && member == "12") {
+        result.value = "24000";
+    } else if (skill == "Java" && member == "13") {
+        result.value = "26000";
+    } else if (skill == "Java" && member == "14") {
+        result.value = "28000";
+    } else if (skill == "Java" && member == "15") {
+        result.value = "30000";
+    } else if (skill == "Java" && member == "16") {
+        result.value = "32000";
+    } else if (skill == "Java" && member == "17") {
+        result.value = "34000";
+    } else if (skill == "Java" && member == "18") {
+        result.value = "36000";
+    } else if (skill == "Java" && member == "19") {
+        result
